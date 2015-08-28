9b7ce39ef40fa365bd00179b0ee244e7cab64344
SpaceIndustries <contact.spaceindustries@gmail.com>
2015-08-28 03:30:30 +0200

    SamPRO-CMS v1
    SamPRO-CMS v1

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="col-md-12">
- <div class="panel panel-default" style="background-color : rgba(0,0,0,0.8); border: black 1px solid;">
-       <div class="panel-body text-center color-white">
-			<h4><i class="fa fa-fw fa-copyright"></i> Copyright <?php echo $copydate ?> développé par <a href="https://github.com/SpaceIndustries" target="_blank">SpaceIndustriesCMS</a> et modifié par <a href="<?php echo $urlsite ?>"><?php echo $websitename ?></a></h4>
-       </div>
- </div>
-</div>
