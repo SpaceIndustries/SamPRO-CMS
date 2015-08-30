798b9131c32ae15c59737d3a71b28b5737e8d7fc
SpaceIndustries <contact.spaceindustries@gmail.com>
2015-08-30 23:59:15 +0200

    SamPRO-CMS v1
    SamPRO-CMS v1

--- includes/teamspeak/teamspeak_infos.php
+++ includes/teamspeak/teamspeak_infos.php
@@ -6,19 +6,18 @@ TeamSpeak3::init();
  
 header('Content-Type: text/html; charset=utf8');
  
-
+$status = "offline";
 $count = 0;
 $max = 0;
  
 try {
-    $ts3 = TeamSpeak3::factory("serverquery://serveradmin:mdp@ip-du-teamspeak:port-du-teamspek/?server_port=port-du-serveur&use_offline_as_virtual=1&no_query_clients=1");
+    $ts3 = TeamSpeak3::factory("serverquery://".$ts3queryuser.":".$ts3querypassword."@".$ipteamspeak3.":".$ts3queryport."/?server_port=".$portteamspeak3."&use_offline_as_virtual=1&no_query_clients=1");
     $status = $ts3->getProperty("virtualserver_status");
     $count = $ts3->getProperty("virtualserver_clientsonline") - $ts3->getProperty("virtualserver_queryclientsonline");
     $max = $ts3->getProperty("virtualserver_maxclients");
 
 }
 catch (Exception $e) {
-    echo '<div style="background-color:red; color:white; display:block; font-weight:bold;">QueryError: ' . $e->getCode() . ' ' . $e->getMessage() . '</div>';
-    $status = "offline";
+    echo ' ';
 } 
 ?>
