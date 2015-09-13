5d867b28e8bbb582f2e3a412076f9c9efa923c60
Faurest Lupine <faurest.lupine.pro@gmail.com>
2015-09-13 22:20:55 +0200

    Debug map.php

--- map.php
+++ map.php
@@ -2,32 +2,33 @@
   <body style="background: url(<?php echo $background ?>) repeat center fixed; margin:0; padding:0; -webkit-background-size: cover; background-size: cover;">
 
     <div class="container">
+    <div class="row ">
 
       <?php include('includes/menu.php'); ?>
 
-	      <div class="panel panel-default" style="background-color : rgba(<?php echo $backgroundrgba; ?>,0.5); border: black 1px solid;">
-			  <div class="panel-body">
+		<div class="grey darken-1 z-depth-2">    
+			 
 			  <!-- Description de la carte interactive -->
-				<div class="col-md-12">
-                <div class="panel panel-default" style="background-color : rgba(0,0,0,0.8); border: black 1px solid;">
+				<div class="col s12">
+                
                     <h1 class="color-white text-center"><i class="fa fa-fw fa-map-marker"></i> Carte du serveur</h1>
                     <div class="panel-body color-white">
 						<?php echo $mapabouts; ?>
                     </div>
-                </div>
+                
                 </div>
 				<!-- iframe gtamap.net -->
-				<div class="col-md-12">
-					<div class="panel panel-default" style="background-color : rgba(0,0,0,0.8); border: black 1px solid;">
-						<div class="panel-body">
-							<h4 class="color-white"><i class="fa fa-fw fa-map-marker"></i> <?php echo $gtamapname; ?></h4><br><center><iframe src="<?php echo $gtamapdotnet; ?>" width="50%" height="50%" frameborder="0"></iframe></center>
-						</div>
-					</div>
+				<div class="col s12">
+						
+							<h4 class="color-white"><i class="fa fa-fw fa-map-marker"></i> <?php echo $gtamapname; ?></h4><br><center><iframe src="<?php echo $gtamapdotnet; ?>" width="100%" height="400" frameborder="0"></iframe></center>
+						
 				</div>
+			
 				<?php require_once('includes/footer.php'); ?>
+				</div>
 			  </div>
 		  </div>
-
+		  </div>
     </div>
 
 <?php require_once('includes/javascript.php'); ?>
